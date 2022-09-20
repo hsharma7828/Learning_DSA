8b21b84f572942dc8f770918985c2bf58463f74e
hsharma7828 <hsharma114@gmail.com>
2022-09-20 12:16:47 +0530

    fixed problem in sum code

--- close_sum.js
+++ close_sum.js
@@ -3,24 +3,28 @@ function getCloser(arr, n) {
     let s_number;
     let sum1;
     let sum;
-    debugger;
     for (i=0; i<n-1; i++) {
         sum1 = arr[i] + arr[i+1]
-        if(i=0) {
+        if(i==0) {
             sum = sum1;
             f_number = arr[i];
             s_number = arr[i+1];
-        } else if (sum > sum1) {
+        } else if (Math.abs(sum) > Math.abs(sum1)) {
             sum = sum1;
             f_number = arr[i];
             s_number = arr[i+1];
         }
     }
-    return (sum,f_number,s_number);
+    let result= {
+        sum:sum,
+        f_number:f_number,
+        s_number:s_number
+    }
+    return result;
 }
 
 arr = [1, 60, -10, 70, -80, 85];
 let result = getCloser(arr, arr.length);
-document.write('Minimun Sum near to Zero is: <br>',result.sum);
-document.write('First Number is: <br>',result.f_number);
-document.write('Second Number is: <br>',result.s_number);
+document.write('Minimun Sum near to Zero is: '+result.sum+'<br>');
+document.write('First Number is: '+result.f_number+'<br>');
+document.write('Second Number is: '+result.s_number+'<br>');
